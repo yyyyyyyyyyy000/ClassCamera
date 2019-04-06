48240c82e1e6046afdac6a7b0112a9544efb0840
无敌帅的yyyyy <248618097@qq.com>
2019-04-06 23:26:19 +0800

    Initial Commit

---
+++ New ClassCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New ClassCamera
+//
+//  Created by 无敌帅的yyyyy on 2019/4/6.
+//  Copyright © 2019 无敌帅的yyyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
